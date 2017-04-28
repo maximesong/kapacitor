3cdb4227d39d609375f323235ed8bc97b4025708
Tim Raymond <tim@timraymond.com>
2017-04-28 10:47:37 -0400

    Use dashes for MQTT configuration
    The configuration style of Kapacitor prefers dashes over underscores.
    This changes the MQTT configuration to align with that style.

--- services/mqtt/config.go
+++ services/mqtt/config.go
@@ -13,12 +13,12 @@ type Config struct {
 	// Port of the MQTT Broker
 	Port uint16 `toml:"port" override:"port"`
 
-	ClientID string `toml:"client_id" override:"client_id"`
+	ClientID string `toml:"client-id" override:"client-id"`
 	Username string `toml:"username" override:"username"`
 	Password string `toml:"password" override:"password"`
 
-	DefaultTopic string   `toml:"default_topic" override:"default_topic"`
-	DefaultQOS   QOSLevel `toml:"default_qos" override:"default_qos"`
+	DefaultTopic string   `toml:"default-topic" override:"default-topic"`
+	DefaultQOS   QOSLevel `toml:"default-qos" override:"default-qos"`
 }
 
 // Broker formats the configured Host and Port as tcp://host:port, suitable for
